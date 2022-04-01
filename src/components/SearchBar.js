75c999ec20629659db0c064289701221cf79b4ac
GoranAviani <goranaviani@gmail.com>
2022-04-01 18:32:06 +0200

    user input with forms and events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 class SearchBar extends React.Component {
     render() {
-        return (<div>
-            <form>
+        return (<div className="ui segment">
+            <form className="ui form">
+                <div className="field">
+                    <label>Image Search</label>
                 <input type="text"/>
-            </form>
+            </div>
+                </form>
         </div>)
     }
 }
