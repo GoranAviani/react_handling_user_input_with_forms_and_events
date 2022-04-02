25d944fef5127e2dd1a4555d72bd4bd2b835b068
GoranAviani <goranaviani@gmail.com>
2022-04-02 12:41:50 +0200

    user input with forms and events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,9 +12,13 @@ class SearchBar extends React.Component {
         this.setState({term: e.target.value})
     }
 
+    onFormSubmit (event) {
+        event.preventDefault();
+    }
+
     render() {
         return (<div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                     <label>Image Search</label>
                     <input type="text"
