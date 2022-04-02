781326d7ebe70bc139a20100f4704c5a8fcf4ee9
GoranAviani <goranaviani@gmail.com>
2022-04-02 12:40:14 +0200

    user input with forms and events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,13 +8,18 @@ class SearchBar extends React.Component {
         console.log(this.state.term)
     }
 
+    userInput = (e) => {
+        this.setState({term: e.target.value})
+    }
+
     render() {
         return (<div className="ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>Image Search</label>
-                    <input type="text" value={this.state.term}
-                           onChange={(e) => this.setState({term: e.target.value.toUpperCase()})}/>
+                    <input type="text"
+                           value={this.state.term}
+                           onChange={this.userInput}/>
                 </div>
             </form>
         </div>)
