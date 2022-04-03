66d6df247c3ea0ddcbef10e841e0091bff4d763b
GoranAviani <goranaviani@gmail.com>
2022-04-03 12:48:09 +0200

    user input with forms and events

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import SearchBar from "./SearchBar";
 
-const App = () => {
-    return (<div>
+class App extends React.Component {
+
+    render() {
+        return (<div>
             <div className="ui container" style={{marginTop: '10px'}}><SearchBar/></div>
         </div>
     )
+    }
+
+
 }
 
 export default App;
